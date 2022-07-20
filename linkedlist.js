12155843bcf5636cb6bbefe5ae2e03c282dc3365
siennalind <sienna.lebeaulind@gmail.com>
2022-07-20 09:55:33 +0100

    constructors for nodes & linked lists

--- linkedlist.js
+++ linkedlist.js
@@ -1,10 +1,21 @@
-// Comment this
-class Node {}
+// Creates a new node of value and sets it pointing at null
+class Node {
+    constructor (value) {
+        this.value = value,
+        this.next = null
+    }
+}
 
-// Comment this
+
 class LinkedList {
-    // Comment this
-    constructor () {}
+    // Creates a new linked list using the constructor for a node, sets both head and
+    // tail pointing at the node and sets the initial length to 1.
+    constructor (value) {
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head
+        this.length = 1
+    }
 
     // push
 
