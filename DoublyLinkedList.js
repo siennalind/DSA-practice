0e69493d69532f2e011bc10a8857ecd0584c451a
siennalind <sienna.lebeaulind@gmail.com>
2022-07-21 18:43:38 +0100

    added push methods

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,40 +1,42 @@
-// Creates a new node of value and sets it pointing at null
 class Node {
-    constructor (value) {
+    constructor(value) {
         this.value = value
         this.next = null
+        this.prev = null
     }
 }
 
-class LinkedList {
-    // Creates a new linked list using the constructor for a node, sets both head and
-    // tail pointing at the node and sets the initial length to 1.
-    constructor (value) {
+class DoublyLinkedList {
+    constructor(value) {
         const newNode = new Node(value)
         this.head = newNode
-        this.tail = this.head
+        this.tail = newNode
         this.length = 1
     }
 
-    // Method to add a node to the end of the list
+    // Method to add a value to the end of the list
     // Returns the list with the new node added
-    push (value) {
+    push(value) {
         // Node to be added
         const newNode = new Node(value)
 
-        // This code runs if the list was empty
+        // This code runs if the list is empty
         if (this.length === 0) {
             this.head = newNode
             this.tail = newNode
         } 
         else {
-            this.tail.next = newNode
+            let temp
+            temp = this.tail
             this.tail = newNode
+            temp.next = newNode
+            newNode.prev = temp
         }
         // This code always runs
         this.length++
         return this
     }
+}
 
     // pop
 
@@ -50,7 +52,4 @@ class LinkedList {
     
     // remove
 
-    // reverse
-}
-
-module.exports = LinkedList;
+module.exports = DoublyLinkedList;
