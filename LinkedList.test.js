14bcf080bde3d788b72bf263bc9e9f4b27455907
siennalind <sienna.lebeaulind@gmail.com>
2022-07-28 21:44:33 +0100

    push

--- LinkedList.test.js
+++ LinkedList.test.js
@@ -11,3 +11,15 @@ describe('#constructor', () => {
     expect(myList.length).toBe(1)
   })
 })
+
+describe('#push', () => {
+  test('adds new node at end of list', () => {
+    const myList = new LinkedList(1)
+    myList.push(2)
+
+    expect(myList.head.value).toBe(1)
+    expect(myList.tail.value).toBe(2)
+    expect(myList.tail.next).toBeNull()
+    expect(myList.length).toBe(2)
+  })
+})
